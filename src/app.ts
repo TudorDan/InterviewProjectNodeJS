5573fcf15284619ec48b4c000bb05cf977278386
TudorDan <tudor.ist@gmail.com>
2021-04-18 15:31:38 +0300

    Add solution for first task

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,38 @@
 import express from 'express';
+import IMedic from './models/medic';
+import chalk from 'chalk';
 
 const app = express();
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+// in memory medic Ids
+let medicIds: string[] = [];
 
 app.get('/', (req, res) => {
     res.send('Hello, Burebista!');
 })
 
-app.listen(9999, () => console.log('Server running'));
+app.post('/', (req, res) => {
+    const medic: IMedic = req.body;
+
+    if (medicIds.includes(medic.id)) {
+        console.log(chalk.inverse.red('Suspicious request: resource id was introduced before!'))
+    } else if (!medic.id) {
+        console.log(chalk.inverse.red('Missing id!'));
+    } else if (medic.resourceType !== 'Practitioner') {
+        console.log(chalk.inverse.red('resourceType is not Practitioner!'));
+        medicIds.push(medic.id);
+    } else if (medic.active) {
+        console.log(chalk.inverse.blueBright('name'));
+        console.log(medic.name);
+        console.log(chalk.inverse.blueBright('facility'));
+        console.log(medic.facility);
+        medicIds.push(medic.id);
+    }
+
+    res.send('Your request was processed');
+})
+
+app.listen(9999, () => console.log('Server running on port: 9999'));
