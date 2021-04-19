eb126183408577c75a676cc1498a9ded87e7ce47
TudorDan <tudor.ist@gmail.com>
2021-04-19 22:57:29 +0300

    Add csv processing

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 import IMedic from './models/medic';
 import chalk from 'chalk';
+import multer from 'multer';
+import fs from 'fs';
+import * as csv from 'fast-csv';
+import path from 'path';
 
 const app = express();
 
@@ -10,29 +14,55 @@ app.use(express.json());
 // in memory medic Ids
 let medicIds: string[] = [];
 
+// CSV file storage
+const upload = multer({ dest: './uploads' });
+
 app.get('/', (req, res) => {
     res.send('Hello, Burebista!');
 })
 
-app.post('/', (req, res) => {
-    const medic: IMedic = req.body;
+app.post('/', upload.single('file'), (req, res) => {
+    if (req.is('json') || req.is('*/json')) {
+        const medic: IMedic = req.body;
 
-    if (medicIds.includes(medic.id)) {
-        console.log(chalk.inverse.red('Suspicious request: resource id was introduced before!'))
-    } else if (!medic.id) {
-        console.log(chalk.inverse.red('Missing id!'));
-    } else if (medic.resourceType !== 'Practitioner') {
-        console.log(chalk.inverse.red('resourceType is not Practitioner!'));
-        medicIds.push(medic.id);
-    } else if (medic.active) {
-        console.log(chalk.inverse.blueBright('name'));
-        console.log(medic.name);
-        console.log(chalk.inverse.blueBright('facility'));
-        console.log(medic.facility);
-        medicIds.push(medic.id);
+        if (medicIds.includes(medic.id)) {
+            console.log(chalk.inverse.red('Suspicious request: resource id was introduced before!'));
+            res.sendStatus(403);
+        } else if (!medic.id) {
+            console.log(chalk.inverse.red('Missing id!'));
+            res.sendStatus(400);
+        } else if (medic.resourceType !== 'Practitioner') {
+            console.log(chalk.inverse.red('resourceType is not Practitioner!'));
+            medicIds.push(medic.id);
+            res.sendStatus(400);
+        } else if (medic.active) {
+            console.log(chalk.inverse.blueBright('name'));
+            console.log(medic.name[0].text);
+            console.log(chalk.inverse.blueBright('facility'));
+            medic.facility.forEach(el => {
+                console.log(el.name);
+            });
+            medicIds.push(medic.id);
+            res.sendStatus(201);
+        }
+    } else {
+        let fileRows: any = [];
+
+        // open uploaded file
+        fs.createReadStream(path.resolve(req.file.path))
+            .pipe(csv.parse({ headers: true }))
+            .on('error', error => console.error(error))
+            .on("data", function (data: any) {
+                fileRows.push(data); // push each row
+            })
+            .on("end", function () {
+                fs.unlinkSync(req.file.path);   // remove temp file
+                //process "fileRows"
+                console.log(fileRows);
+            });
+
+        res.sendStatus(201);
     }
-
-    res.send('Your request was processed');
 })
 
 app.listen(9999, () => console.log('Server running on port: 9999'));
